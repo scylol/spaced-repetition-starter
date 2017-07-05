2129d4c210c39fac0ae2b4945bc33fa2d504a46a
tachyla <tachylad.murray@gmail.com>
2017-07-05 08:33:46 -0700

    added <p>

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -7,6 +7,7 @@ export default function LoginPage() {
     return(
     <div className='login'>
         <h1>{text}</h1>
+        <p>Bienvenue is an exciting app where you can learn French! Signup or Login below to get started</p>
         <a href={'/api/auth/google'}>Login with Google</a>
     </div>
     );
