680bf7d59290a4bd6cbd22f2ce36c5ebfe6a816c
tachyla <tachylad.murray@gmail.com>
2017-07-05 08:06:38 -0700

    added welcome header

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+
 export default function LoginPage() {
-    return <a href={'/api/auth/google'}>Login with Google</a>;
+    const text  = 'Welcome! Bienvenue!';
+
+    return(
+    <div className='login'>
+        <h1>{text}</h1>
+        <a href={'/api/auth/google'}>Login with Google</a>
+    </div>
+    );
 }
