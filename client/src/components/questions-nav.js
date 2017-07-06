82b950809648d0e9cc1ff01f38f6fe90a1b0d176
tachyla <tachylad.murray@gmail.com>
2017-07-05 23:52:09 -0700

    add questions navbar

---
+++ client/src/components/questions-nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { logout } from '../actions/actions';
+import '../styles/questions-nav.css';
+
+
+export default function QuestionsNavbar() {
+    const navBarHeader = 'Questions Nav Bar';
+    const button = 'Log Out';
+
+    return(
+        <div className='navBarHeader'>
+            <h1>{navBarHeader}</h1>
+            <button className='login_button'>
+                {button}
+            </button>
+        </div>
+        );
+}
